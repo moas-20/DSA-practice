a395a097be59a7bf7cf1d312bd787490d675016d
moas-20 <mk333hot@gmail.com>
2026-08-27 05:12:06 +0200

    added fib & fir function

--- Recursive/script.js
+++ Recursive/script.js
@@ -6,7 +6,7 @@ function fib(num){
         return seq.slice(0,num);
     }
 
-    for(let i=2; i <= num; i++){
+    for(let i=2; i < num; i++){
         seq[i] = seq[i-1] + seq[i-2];
     }
 
@@ -25,7 +25,7 @@ function fir(num,seq=[0,1]){
     if(num <= 1){
         return seq.slice(0,num +1);
     }
-    if(num >=2){
+    if(num > 2){
         let newNum = seq.at(-1) + seq.at(-2);
         seq.push(newNum);
         fir(num -1,seq);
